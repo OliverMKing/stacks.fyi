b38828511f5a5ea42f951ed0566b33fbd408d2ff
Bryce <brycekane5@gmail.com>
2020-08-10 18:19:36 -0400

    create search page

--- frontend/src/components/home.tsx
+++ frontend/src/components/home.tsx
@@ -1,25 +1,47 @@
 
 import React from 'react';
+import blob from './blob.svg'
+import { url } from 'inspector';
 
+const styles = {
+    background_image: {
+        backgroundImage: 'url(' + blob + ')',
+        backgroundRepeat: "no-repeat",
+        height: '1000px',
+        width: '100%'
 
+    } as React.CSSProperties,
+}
 const App: React.FC = () => {
     return (
-        <div className="m-auto antialiased font-sans font-serif font-mono text-center">
-            <header className="bg-gray-900 min-h-screen flex flex-col items-center justify-center text-white text-2xl">
-                <img className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-                <a
-                    className="text-blue-300"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn Taiwlind with React TypeScript
-        </a>
-            </header>
+        <div style={styles.background_image}>
+            <div className="m-auto">
+
+
+
+                <div className="flex mb-4">
+                    <div className="w-full  h-48"></div>
+                </div>
+                <div className="flex mb-2">
+                    <div className="w-1/4 h-12"></div>
+                    <div className="w-1/2  h-12">
+
+                        <form className="w-full max-w-sm">
+
+                            <div className="flex items-center space-x-2">
+                                <input className="bg-white focus:outline-none border border-gray-500 rounded-lg py-3 px-4 block w-full appearance-none leading-normal" type="address" placeholder="Charlotte, NC" />
+
+                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                                    Search
+                    </button>
+                            </div>
+                        </form>
+                    </div>
+
+                </div></div>
+
         </div>
+
     );
 }
 
