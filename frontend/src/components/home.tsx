28f4673d36b96d36fa1b9841011af5cf4c8101e6
Bryce <brycekane5@gmail.com>
2020-08-10 16:38:01 -0400

    added react-router-dom fixes issue #7

---
+++ frontend/src/components/home.tsx
@@ -0,0 +1,26 @@
+
+import React from 'react';
+
+
+const App: React.FC = () => {
+    return (
+        <div className="m-auto antialiased font-sans font-serif font-mono text-center">
+            <header className="bg-gray-900 min-h-screen flex flex-col items-center justify-center text-white text-2xl">
+                <img className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+                <a
+                    className="text-blue-300"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn Taiwlind with React TypeScript
+        </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
