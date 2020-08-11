d2bfe2b8f221ad06f8fe8588337ed5ff80d2925d
Bryce <brycekane5@gmail.com>
2020-08-11 16:47:21 -0400

    style search page

--- frontend/src/components/home.tsx
+++ frontend/src/components/home.tsx
@@ -1,46 +1,56 @@
 
 import React from 'react';
 import blob from './blob.svg'
+import working from './working.svg'
 import { url } from 'inspector';
 
 const styles = {
     background_image: {
         backgroundImage: 'url(' + blob + ')',
         backgroundRepeat: "no-repeat",
-        height: '1000px',
-        width: '100%'
+        height: '3000px'
+
+    } as React.CSSProperties,
+}
+const styles2 = {
+    background_image: {
+        backgroundImage: 'url(' + working + ')',
+        backgroundRepeat: "no-repeat",
+        height: '3000px'
 
     } as React.CSSProperties,
 }
 const App: React.FC = () => {
     return (
         <div style={styles.background_image}>
-            <div className="m-auto">
 
-
-
-                <div className="flex mb-4">
-                    <div className="w-full  h-48"></div>
-                </div>
-                <div className="flex mb-2">
-                    <div className="w-1/4 h-12"></div>
-                    <div className="w-1/2  h-12">
-
-                        <form className="w-full max-w-sm">
-
+            <div className="grid grid-cols-3 gap-4">
+                <div className="col-span-1 ml-20">
+                    <div className="m-auto">
+                        <div className="flex">
+                            <div className="w-full  h-20"></div>
+                        </div>
+                        <div className="text-gray-900 font-bold text-4xl mb-2">Search popular technology stacks by city</div>
+                        <form className="w-full max-w-lg">
                             <div className="flex items-center space-x-2">
                                 <input className="bg-white focus:outline-none border border-gray-500 rounded-lg py-3 px-4 block w-full appearance-none leading-normal" type="address" placeholder="Charlotte, NC" />
-
                                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
                                     Search
-                    </button>
+                               </button>
                             </div>
                         </form>
+
                     </div>
+                </div>
+                <div className="col-span-2">
+                    <div style={styles2.background_image}>
+                    </div>
+                    <h1>test</h1>
+                </div>
+            </div>
 
-                </div></div>
 
-        </div>
+        </div >
 
     );
 }
