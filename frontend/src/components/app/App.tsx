17fd2c1cdaef54467c581da084c9058ad22d51f5
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2020-08-13 14:26:04 -0400

    Modify navbar

--- frontend/src/components/app/App.tsx
+++ frontend/src/components/app/App.tsx
@@ -8,58 +8,23 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <div>
-          <nav className="flex items-center justify-between flex-wrap bg-white-500 p-6">
-            <div className="flex items-center flex-shrink-0 text-black mr-6">
-              <span className="font-semibold text-xl tracking-tight">
-                Stacks.fyi
-              </span>
-            </div>
-            <div className="block lg:hidden">
-              <button className="flex items-center px-3 py-2 border rounded text-blue-200 border-blue-400 hover:text-white hover:border-white">
-                <svg
-                  className="fill-current h-3 w-3"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
+        <div className="w-full container mx-auto">
+          <nav className="flex items-center justify-between bg-white-500 py-8">
+            <span className="font-semibold text-3xl tracking-tight">
+              <a href="/">Stacks.fyi</a>
+            </span>
+            <div className="block flex items-center w-auto">
+              <div className="flex-grow">
+                <button
+                  id="navAction"
+                  className="mx-auto bg-blue-700 hover:bg-blue-900 text-white text-sm rounded py-2 px-8 shadow"
+                  onClick={() => (window.location.href = "/location-search")}
                 >
-                  <title>Menu</title>
-                  <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-                </svg>
-              </button>
-            </div>
-            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-              <div className="text-sm lg:flex-grow">
-                <a
-                  href="#responsive-header"
-                  className="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white mr-4"
-                >
-                  Docs
-                </a>
-                <a
-                  href="#responsive-header"
-                  className="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white mr-4"
-                >
-                  Examples
-                </a>
-                <a
-                  href="#responsive-header"
-                  className="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white"
-                >
-                  Blog
-                </a>
-              </div>
-              <div>
-                <a
-                  href="#"
-                  className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0"
-                >
-                  Download
-                </a>
+                  Stacks By Location
+                </button>
               </div>
             </div>
           </nav>
-
-          <hr />
 
           <Switch>
             <Route exact path="/location-search">
