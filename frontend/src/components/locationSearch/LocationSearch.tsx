c25b1aa57c08e59bedf4c071b10d0ac49d8f58a2
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2020-08-16 18:05:07 -0400

    Add location search content

--- frontend/src/components/locationSearch/LocationSearch.tsx
+++ frontend/src/components/locationSearch/LocationSearch.tsx
@@ -3,57 +3,75 @@ import blob from "./blob.svg";
 import working from "./working.svg";
 import NavbarComponent from "../navbar/Navbar";
 
-const styles = {
-  background_image: {
-    backgroundImage: "url(" + blob + ")",
-    backgroundRepeat: "no-repeat",
-    height: "3000px",
-  } as React.CSSProperties,
-};
-const styles2 = {
-  background_image: {
-    backgroundImage: "url(" + working + ")",
-    backgroundRepeat: "no-repeat",
-    height: "3000px",
-  } as React.CSSProperties,
-};
-const LocationSearchComponent: React.FC = () => {
-  return (
-    <div className="w-full container mx-auto">
-      <NavbarComponent />
+interface Search {
+  name: string;
+}
 
-      <div style={styles.background_image}>
-        <div className="grid grid-cols-3 gap-4">
-          <div className="col-span-1 ml-20">
-            <div className="m-auto">
-              <div className="flex">
-                <div className="w-full  h-20"></div>
-              </div>
-              <div className="text-gray-900 font-bold text-4xl mb-2">
-                Search popular technology stacks by city
-              </div>
-              <div className="w-full text-center pt-4">
-                <form action="#">
-                  <div className="max-w-xs md:max-w-sm mx-auto p-1 bg-white rounded shadow flex flex-wrap items-center">
-                    <input
-                      type="text"
-                      placeholder="Location (Ex: Raleigh, NC)"
-                      className="flex-1 appearance-none p-3 text-gray-600 mr-2"
-                    />
-                    <button
-                      type="submit"
-                      className="flex-2 block md:inline-block appearance-none text-white text-base font-semibold tracking-wider p-3 rounded bg-red-400 hover:bg-red-500"
-                    >
-                      Search
+const LocationSearchComponent: React.FC = () => {
+  // Describes common searches, add new ones here
+  const commonSearches: Search[] = [
+    { name: "United States" },
+    { name: "San Francisco, CA" },
+    { name: "Irvine, CA" },
+    { name: "New York, NY" },
+    { name: "Washington, D.C." },
+    { name: "Chicago, IL" },
+    { name: "Austin, TX" },
+    { name: "Dallas, TX" },
+    { name: "Raleigh, NC" },
+    { name: "Atlanta, GA" },
+    { name: "Boston, MA" },
+    { name: "Seattle, WA" },
+  ];
+
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <div className="w-full container mx-auto">
+        <NavbarComponent />
+        <div className="container max-w-5xl mx-auto m-8 pt-8">
+          <h1 className="w-full my-2 text-4xl font-bold leading-tight text-center text-gray-800">
+            Search stacks by location
+          </h1>
+          <h3 className="break-normal font-normal text-gray-800 text-center text-xl">
+            Search by City, State, or Country
+          </h3>
+          <div className="w-full pt-8">
+            <form action="#">
+              <div className="max-w-xs md:max-w-xl mx-auto p-1 bg-white rounded shadow flex flex-wrap items-center">
+                <input
+                  type="text"
+                  placeholder="Location (Ex: Raleigh, NC)"
+                  className="flex-1 appearance-none p-3 text-gray-600 mr-2"
+                />
+                <button
+                  type="submit"
+                  className="flex-2 block md:inline-block appearance-none bg-blue-600 text-white text-base font-semibold tracking-wider p-3 rounded hover:bg-blue-500"
+                >
+                  Search
                 </button>
-                  </div>
-                </form>
               </div>
+            </form>
+          </div>
+          <div className="w-full pt-16">
+            <h1 className="w-full my-2 text-4xl font-bold leading-tight text-center text-gray-800">
+              Common searches
+            </h1>
+            <div className="flex flex-wrap pt-4 content-center">
+              {commonSearches.map((search) => {
+                return (
+                  <div className="w-1/2 md:w-1/3 p-3">
+                    <button className="w-full bg-gray-200 hover:bg-gray-300 p-4 rounded">
+                      {search.name}
+                    </button>
+                  </div>
+                );
+              })}
             </div>
           </div>
-          <div className="col-span-2">
-            <div style={styles2.background_image}></div>
-            <h1>test</h1>
+          <div className="max-w-5xl text-center mt-4 md:mt-8 pt-4 sm:border-t">
+            <p className="text-gray-600">
+              © Stacks.fyi 2020. All Rights Reserved.
+            </p>
           </div>
         </div>
       </div>
