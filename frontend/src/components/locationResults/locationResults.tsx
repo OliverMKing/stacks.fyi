ae73f355966a85a005eb471a0fba3acacf0be360
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2020-08-19 18:59:53 -0400

    Fix order of podium on small screens

--- frontend/src/components/locationResults/locationResults.tsx
+++ frontend/src/components/locationResults/locationResults.tsx
@@ -47,6 +47,21 @@ const App: React.FunctionComponent<RouteComponentProps> = (props) => {
 
   const ranking = (first: string, second: string, third: string) => (
     <div className="flex flex-col sm:flex-row justify-center pt-12 my-12 sm:my-4 ">
+      <div className="sm:hidden flex flex-col w-5/6 lg:w-1/3 mx-auto lg:mx-0 rounded-lg bg-white mt-4 sm:-mt-6 shadow-lg z-10">
+        <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+          <div className="flex items-center justify-center">
+            <img
+              src="https://img.icons8.com/emoji/96/000000/1st-place-medal-emoji.png"
+              alt="1st place medal"
+            />
+          </div>
+          <div className="w-full p-8 text-3xl font-bold text-center">
+            {first}
+          </div>
+          <div className="h-1 w-full gradient my-0 py-0 rounded-t"></div>
+        </div>
+      </div>
+
       <div className="flex flex-col w-5/6 lg:w-1/4 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-white mt-4 shadow-md z-5">
         <div className="flex-1 bg-white text-gray-700 rounded-t rounded-b-none overflow-hidden shadow">
           <div className="flex items-center justify-center">
@@ -59,7 +74,7 @@ const App: React.FunctionComponent<RouteComponentProps> = (props) => {
         </div>
       </div>
 
-      <div className="flex flex-col w-5/6 lg:w-1/3 mx-auto lg:mx-0 rounded-lg bg-white mt-4 sm:-mt-6 shadow-lg z-10">
+      <div className="hidden sm:flex flex-col w-5/6 lg:w-1/3 mx-auto lg:mx-0 rounded-lg bg-white mt-4 sm:-mt-6 shadow-lg z-10">
         <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
           <div className="flex items-center justify-center">
             <img
@@ -91,7 +106,7 @@ const App: React.FunctionComponent<RouteComponentProps> = (props) => {
   return (
     <div className="bg-gray-100 min-h-screen pb-3">
       <div className="w-full container mx-auto">
-        <NavbarComponent />`
+        <NavbarComponent />
         <div className="container max-w-5xl mx-auto m-7 pt-8">
           <section className="py-8">
             <div className="container mx-auto px-2 pt-4 pb-12 text-gray-800">
