b8839e5182f8571bce4ea40aec470074c696baf4
Bryce <brycekane5@gmail.com>
2020-08-19 13:43:15 -0400

    linking of search to results page

--- frontend/src/components/locationResults/locationResults.tsx
+++ frontend/src/components/locationResults/locationResults.tsx
@@ -1,7 +1,9 @@
 import React, { Props } from "react";
 import NavbarComponent from "../navbar/Navbar";
 import { RouteComponentProps } from "react-router-dom";
-import * as QueryString from "query-string"
+import * as QueryString from "query-string";
+import blob from "./blob.svg";
+
 
 const styles = {
     background_image: {
@@ -12,7 +14,7 @@ const styles = {
 };
 const styles2 = {
     background_image: {
-
+        backgroundImage: "url(" + blob + ")",
         backgroundRepeat: "no-repeat",
         height: "3000px",
     } as React.CSSProperties,
@@ -23,11 +25,101 @@ const App: React.FunctionComponent<RouteComponentProps> = props => {
     console.log(params.q); // "shoes"
     console.log(params.clearance); // true
     return (
-        <div><h1>
-            we out here
-       {params.q}
 
-        </h1></div>
+        <div className="bg-gray-100 min-h-screen pb-3">
+
+
+            <div className="w-full container mx-auto">
+
+                <NavbarComponent />
+
+                <div className="container max-w-5xl mx-auto m-7 pt-8">
+
+                    <section className="bg-gray-100 py-8">
+
+
+
+                        <div className="container mx-auto px-2 pt-4 pb-12 text-gray-800">
+
+                            <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">{params.q}</h1>
+                            <div className="w-full mb-4">
+                                <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+                            </div>
+
+
+
+                            <div className="flex flex-col sm:flex-row justify-center pt-12 my-12 sm:my-4">
+
+                                <div className="flex flex-col w-5/6 lg:w-1/4 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-white mt-4">
+                                    <div className="flex-1 bg-white text-gray-600 rounded-t rounded-b-none overflow-hidden shadow">
+                                        <div className="p-8 text-3xl font-bold text-center border-b-4">Node.js</div>
+                                        <ul className="w-full text-center text-sm">
+                                            <li className="border-b py-4">398 Job Postings</li>
+                                            <li className="border-b py-4">29 Unique Companies</li>
+                                            <li className="border-b py-4">70,000$ Average Salary</li>
+                                        </ul>
+                                    </div>
+                                    <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
+                                        <div className="w-full pt-6 text-3xl text-gray-600 font-bold text-center">7,413<span className="text-base"> Job Postings</span></div>
+                                        <div className="flex items-center justify-center">
+                                            <button className="mx-auto lg:mx-0 hover:underline gradient text-black font-bold rounded-full my-6 py-4 px-8 shadow-lg">2</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+
+                                <div className="flex flex-col w-5/6 lg:w-1/3 mx-auto lg:mx-0 rounded-lg bg-white mt-4 sm:-mt-6 shadow-lg z-10">
+                                    <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+                                        <div className="w-full p-8 text-3xl font-bold text-center">React.js</div>
+                                        <div className="h-1 w-full gradient my-0 py-0 rounded-t"></div>
+                                        <ul className="w-full text-center text-base font-bold">
+                                            <li className="border-b py-4">4,694 Job Postings</li>
+                                            <li className="border-b py-4">53 Unique Companies</li>
+                                            <li className="border-b py-4">83,000$ Average Salary</li>
+                                            <li className="border-b py-4">Thing</li>
+                                        </ul>
+                                    </div>
+                                    <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
+                                        <div className="w-full pt-6 text-4xl font-bold text-center">10,241 <span className="text-base"> Job Postings</span></div>
+                                        <div className="flex items-center justify-center">
+                                            <button className="mx-auto lg:mx-0 hover:underline gradient text-black font-bold rounded-full my-6 py-4 px-8 shadow-lg">1</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+
+                                <div className="flex flex-col w-5/6 lg:w-1/4 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-white mt-4">
+                                    <div className="flex-1 bg-white text-gray-600 rounded-t rounded-b-none overflow-hidden shadow">
+                                        <div className="p-8 text-3xl font-bold text-center border-b-4">C++</div>
+                                        <ul className="w-full text-center text-sm">
+                                            <li className="border-b py-4">10,000 Job Postings</li>
+                                            <li className="border-b py-4">400 Unique Companies</li>
+                                            <li className="border-b py-4">70,000$ Average Salary</li>
+                                        </ul>
+                                    </div>
+                                    <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
+                                        <div className="w-full pt-6 text-3xl text-gray-600 font-bold text-center">7,413<span className="text-base"> Job Postings</span></div>
+                                        <div className="flex items-center justify-center">
+                                            <button className="mx-auto lg:mx-0 hover:underline  text-black font-bold rounded-full my-6 py-4 px-8 shadow-lg">3</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+
+                    </section>
+
+
+                </div>
+
+            </div>
+        </div>
+
     )
 }
 
--- frontend/src/components/locationSearch/LocationSearch.tsx
+++ frontend/src/components/locationSearch/LocationSearch.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import NavbarComponent from "../navbar/Navbar";
+import { useHistory } from 'react-router-dom';
 
 interface Search {
   name: string;
 }
 
 const LocationSearchComponent: React.FC = () => {
+  const history = useHistory();
+
   // Describes common searches, add new ones here
   const commonSearches: Search[] = [
     { name: "United States" },
@@ -21,7 +24,14 @@ const LocationSearchComponent: React.FC = () => {
     { name: "Boston, MA" },
     { name: "Seattle, WA" },
   ];
-
+  const [searchparam, setsearchparam] = useState("test");
+  const changeSearch = ({ target }: { target: any }) => {
+    setsearchparam(target.value)
+    console.log(searchparam)
+  }
+  const submitform = () => {
+    history.push("/location-results/:?q=" + searchparam);
+  }
   return (
     <div className="bg-gray-100 min-h-screen pb-3">
       <div className="w-full container mx-auto">
@@ -34,9 +44,10 @@ const LocationSearchComponent: React.FC = () => {
             Search by City, State, or Country
           </h3>
           <div className="w-full pt-8">
-            <form action="#">
+            <form onSubmit={submitform}>
               <div className="max-w-xs md:max-w-xl mx-auto p-1 bg-white rounded shadow flex flex-wrap items-center">
                 <input
+                  onChange={changeSearch}
                   type="text"
                   placeholder="Location (Ex: Raleigh, NC)"
                   className="flex-1 appearance-none p-3 text-gray-600 mr-2"
@@ -44,7 +55,7 @@ const LocationSearchComponent: React.FC = () => {
                 <button
                   type="submit"
                   className="flex-2 block md:inline-block appearance-none bg-blue-600 text-white text-base font-semibold tracking-wider p-3 rounded hover:bg-blue-500"
-                >
+                  onClick={() => (window.location.href = "/location-results/:?q=" + searchparam)}>
                   Search
                 </button>
               </div>
@@ -58,7 +69,7 @@ const LocationSearchComponent: React.FC = () => {
               {commonSearches.map((search) => {
                 return (
                   <div className="w-1/2 md:w-1/3 p-3">
-                    <button className="w-full bg-gray-200 hover:bg-gray-300 p-4 rounded">
+                    <button className="w-full bg-gray-200 hover:bg-gray-300 p-4 rounded" onClick={() => (window.location.href = "/location-results/:?q=" + search.name)}>
                       {search.name}
                     </button>
                   </div>
