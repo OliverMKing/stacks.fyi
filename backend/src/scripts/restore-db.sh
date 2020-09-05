4c86e22740377f8bfe54372eedc63069887351be
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2020-09-04 21:09:41 -0400

    Add restore db script

--- backend/src/scripts/restore-db.sh
+++ backend/src/scripts/restore-db.sh
@@ -1,8 +1,8 @@
 set -e
 # https://stackoverflow.com/questions/24718706/backup-restore-a-dockerized-postgresql-database
-
 SERVER="my_database_server";
 PW="mysecretpassword";
 DB="my_database";
-echo "Backing up docker postgres [$SERVER]..."
-docker exec -t $SERVER pg_dumpall -c -U postgres > backup.sql
+echo "Restoring docker postgres [$SERVER]..."
+
+cat backup.sql | docker exec -i $SERVER psql -U postgres
